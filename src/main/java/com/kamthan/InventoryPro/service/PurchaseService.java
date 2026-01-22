fcc22c562e0e9f98f5ec6122672c95c8579ac2cd
Priyansh Kamthan <priyansh.kamthan123@gmail.com>
2026-01-22 16:23:21 +0530

    added interface: Purchase and sale service

--- src/main/java/com/kamthan/InventoryPro/service/PurchaseService.java
+++ src/main/java/com/kamthan/InventoryPro/service/PurchaseService.java
@@ -1,144 +1,15 @@
 package com.kamthan.InventoryPro.service;
 
 import com.kamthan.InventoryPro.dto.PurchaseResponseDTO;
-import com.kamthan.InventoryPro.exception.InvalidRequestException;
-import com.kamthan.InventoryPro.exception.ResourceNotFoundException;
-import com.kamthan.InventoryPro.mapper.PurchaseMapper;
-import com.kamthan.InventoryPro.model.Product;
 import com.kamthan.InventoryPro.model.Purchase;
-import com.kamthan.InventoryPro.model.PurchaseItem;
-import com.kamthan.InventoryPro.model.enums.MovementType;
-import com.kamthan.InventoryPro.model.enums.ReferenceType;
-import com.kamthan.InventoryPro.repository.ProductRepository;
-import com.kamthan.InventoryPro.repository.PurchaseRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Service
-public class PurchaseService {
-    @Autowired
-    private PurchaseRepository purchaseRepository;
-    @Autowired
-    private PurchaseMapper purchaseMapper;
-    @Autowired
-    private ProductRepository productRepository;
-    @Autowired
-    private StockMovementService stockMovementService;
+public interface PurchaseService {
+    public Purchase addPurchase(Purchase purchase);
 
-    @Transactional
-    public Purchase addPurchase(Purchase purchase) {
-        double totalAmount = 0.0;
-        double totalTax = 0.0;
+    public List<Purchase> getAllPurchases();
 
-        if (purchase.getItems() == null || purchase.getItems().isEmpty()) {
-            throw new IllegalArgumentException("Purchase must contain at least one item");
-        }
-
-        Map<Long, Product> productCache = new HashMap<>();
-
-        for (PurchaseItem item : purchase.getItems()) {
-            Long productId = item.getProduct().getId();
-            if (productId == null) {
-                throw new IllegalArgumentException("Product id is required for each purchase item");
-            }
-
-            Product product = productCache.computeIfAbsent(productId,
-                    id -> productRepository.findById(id)
-                            .orElseThrow(() -> new RuntimeException("Product not found with id: " + id)));
-
-            if (item.getQuantity() == null || item.getQuantity() <= 0) {
-                throw new IllegalArgumentException("Quantity must be > 0 for product id: " + productId);
-            }
-
-            int before = product.getQuantity() == null ? 0 : product.getQuantity();
-            int qty = item.getQuantity();
-
-            if (item.getPricePerUnit() == null) {
-                item.setPricePerUnit(product.getPrice());
-            }
-
-            double tax = item.getTaxAmount() != null ? item.getTaxAmount()
-                    : item.getPricePerUnit() * qty * 0.1;
-            item.setTaxAmount(tax);
-
-            totalTax += tax;
-            totalAmount += item.getPricePerUnit() * qty + tax;
-
-            product.setQuantity(before + qty);
-            productCache.put(productId, product);
-
-            item.setPurchase(purchase);
-        }
-
-        purchase.setTotalAmount(totalAmount);
-        purchase.setTaxAmount(totalTax);
-        purchase.setPurchaseDate(LocalDateTime.now());
-
-        Purchase savedPurchase = purchaseRepository.save(purchase);
-
-        productRepository.saveAll(productCache.values());
-
-        for (PurchaseItem item : savedPurchase.getItems()) {
-            Long productId = item.getProduct().getId();
-            Product product = productCache.get(productId);
-
-            int after = product.getQuantity();
-            int qty = item.getQuantity();
-            int before = after - qty;
-
-            stockMovementService.recordMovement(
-                    product,
-                    MovementType.IN,
-                    qty,
-                    before,
-                    after,
-                    ReferenceType.PURCHASE,
-                    savedPurchase.getId()
-            );
-        }
-
-        return savedPurchase;
-    }
-
-    public List<Purchase> getAllPurchases() {
-        return purchaseRepository.findAll();
-    }
-
-    public List<PurchaseResponseDTO> getPurchasesByDateRange(
-            LocalDate from,
-            LocalDate to
-    ) {
-        if (from == null || to == null) {
-            throw new InvalidRequestException("From date and To date are required");
-        }
-
-        if (from.isAfter(to)) {
-            throw new InvalidRequestException("From date cannot be after To date");
-        }
-
-        LocalDateTime start = from.atStartOfDay();
-        LocalDateTime end = to.atTime(23, 59, 59);
-
-        List<Purchase> purchases =
-                purchaseRepository.findByPurchaseDateBetween(start, end);
-
-        if (purchases.isEmpty()) {
-            throw new ResourceNotFoundException(
-                    "No purchases found between " + from + " and " + to
-            );
-        }
-
-        return purchases.stream()
-                .map(purchaseMapper::toResponseDTO)
-                .toList();
-    }
-
-
+    public List<PurchaseResponseDTO> getPurchasesByDateRange(LocalDate from, LocalDate to);
 }
--- src/main/java/com/kamthan/InventoryPro/service/SaleService.java
+++ src/main/java/com/kamthan/InventoryPro/service/SaleService.java
@@ -1,153 +1,17 @@
 package com.kamthan.InventoryPro.service;
 
-import com.kamthan.InventoryPro.dto.SaleItemResponseDTO;
 import com.kamthan.InventoryPro.dto.SaleResponseDTO;
-import com.kamthan.InventoryPro.exception.InsufficientStockException;
-import com.kamthan.InventoryPro.exception.InvalidRequestException;
-import com.kamthan.InventoryPro.exception.ResourceNotFoundException;
-import com.kamthan.InventoryPro.mapper.SaleMapper;
-import com.kamthan.InventoryPro.model.Product;
 import com.kamthan.InventoryPro.model.Sale;
-import com.kamthan.InventoryPro.model.SaleItem;
-import com.kamthan.InventoryPro.model.StockMovement;
-import com.kamthan.InventoryPro.model.enums.MovementType;
-import com.kamthan.InventoryPro.model.enums.ReferenceType;
-import com.kamthan.InventoryPro.repository.ProductRepository;
-import com.kamthan.InventoryPro.repository.SaleRepository;
 import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Service
-public class SaleService {
-    @Autowired
-    private SaleRepository saleRepository;
-
-    @Autowired
-    private SaleMapper saleMapper;
-    @Autowired
-    private ProductRepository productRepository;
-    @Autowired
-    private StockMovementService stockMovementService;
-
-    // add sale method
+public interface SaleService {
     @Transactional
-    public Sale addSale(Sale sale) {
-        double totalAmount = 0.0;
-        double totalTax = 0.0;
+    public Sale addSale(Sale sale);
 
-        // validate items
-        if (sale.getItems() == null || sale.getItems().isEmpty()) {
-            throw new IllegalArgumentException("Sale must contain at least one item");
-        }
+    public List<Sale> getAllSales();
 
-        // cache products to avoid multiple DB hits
-        Map<Long, Product> productCache = new HashMap<>();
-
-        // First pass: validate and prepare
-        for (SaleItem item : sale.getItems()) {
-            Long productId = item.getProduct() == null ? null : item.getProduct().getId();
-            if (productId == null) {
-                throw new ResourceNotFoundException("Product id is required for each sale item");
-            }
-
-            Product product = productCache.computeIfAbsent(productId,
-                    id -> productRepository.findById(id)
-                            .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + id)));
-
-            if (item.getQuantity() == null || item.getQuantity() <= 0) {
-                throw new InsufficientStockException("Quantity must be > 0 for product id: " + productId);
-            }
-
-            int available = product.getQuantity() == null ? 0 : product.getQuantity();
-            int qty = item.getQuantity();
-            if (available < qty) {
-                throw new InsufficientStockException("Insufficient stock for product id: " + productId +
-                        ". Available: " + available + ", requested: " + qty);
-            }
-
-            // set pricePerUnit from product master if not provided
-            if (item.getPricePerUnit() == null) {
-                item.setPricePerUnit(product.getPrice());
-            }
-
-            // compute tax if not provided (default 10%)
-            double tax = item.getTaxAmount() != null ? item.getTaxAmount()
-                    : item.getPricePerUnit() * qty * 0.1;
-            item.setTaxAmount(tax);
-
-            totalTax += tax;
-            totalAmount += item.getPricePerUnit() * qty + tax;
-
-            // update product quantity in cache (decrement)
-            product.setQuantity(available - qty);
-            productCache.put(productId, product);
-
-            // associate item with sale (so cascade persists items)
-            item.setSale(sale);
-        }
-
-        sale.setTotalAmount(totalAmount);
-        sale.setTaxAmount(totalTax);
-        sale.setSaleDate(LocalDateTime.now());
-
-        // Save sale (ensure cascade = ALL on items)
-        Sale savedSale = saleRepository.save(sale);
-
-        productRepository.saveAll(productCache.values());
-
-        for (SaleItem item : savedSale.getItems()) {
-            Long productId = item.getProduct().getId();
-            Product product = productCache.get(productId);
-
-            int after = product.getQuantity();
-            int qty = item.getQuantity();
-            int before = after + qty;
-
-            stockMovementService.recordMovement(
-                    product,
-                    MovementType.OUT,
-                    qty,
-                    before,
-                    after,
-                    ReferenceType.SALE,
-                    savedSale.getId()
-            );
-        }
-
-        return savedSale;
-    }
-
-    public List<Sale> getAllSales() {
-        return saleRepository.findAll();
-    }
-
-    public List<SaleResponseDTO> getSalesByDateRange(LocalDate from, LocalDate to) {
-        if(from == null || to == null) {
-            throw new InvalidRequestException("From date and To date are required");
-        }
-        if(from.isAfter(to)) {
-            throw new InvalidRequestException("From date can not be after To date");
-        }
-
-        LocalDateTime start = from.atStartOfDay();
-        LocalDateTime end = to.atTime(23,59,59);
-
-        List<Sale> sales = saleRepository.findBySaleDateBetween(start, end);
-
-        if(sales.isEmpty()) throw new ResourceNotFoundException("No sales found between "+from+" and "+to);
-
-        return sales.stream()
-                .map(saleMapper::toResponseDTO)
-                .toList();
-    }
+    public List<SaleResponseDTO> getSalesByDateRange(LocalDate from, LocalDate to);
 }
