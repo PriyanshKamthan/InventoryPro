6add69fcebeab9fd2a395118b74e477e2e10ab4e
Priyansh Kamthan <priyansh.kamthan@gmail.com>
2026-01-22 14:05:21 +0530

    added logs: ProductService

--- src/main/java/com/kamthan/InventoryPro/service/ProductService.java
+++ src/main/java/com/kamthan/InventoryPro/service/ProductService.java
@@ -2,9 +2,11 @@ package com.kamthan.InventoryPro.service;
 
 import com.kamthan.InventoryPro.dto.ProductReportDTO;
 import com.kamthan.InventoryPro.dto.ProductResponseDTO;
+import com.kamthan.InventoryPro.exception.InvalidRequestException;
 import com.kamthan.InventoryPro.exception.ResourceNotFoundException;
 import com.kamthan.InventoryPro.mapper.ProductMapper;
 import com.kamthan.InventoryPro.model.Product;
+import com.kamthan.InventoryPro.model.StockMovement;
 import com.kamthan.InventoryPro.model.enums.MovementType;
 import com.kamthan.InventoryPro.model.enums.ReferenceType;
 import com.kamthan.InventoryPro.model.enums.UnitOfMeasure;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -33,70 +36,107 @@ public class ProductService {
         }
         int openingQty = product.getQuantity() != null ? product.getQuantity() : 0;
         product.setQuantity(openingQty);
+
+        log.info("Adding product: name={}, category={}, initialQty={}",
+                product.getName(), product.getCategory(), product.getQuantity());
+
         Product savedProduct = productRepository.save(product);
+
         if (openingQty > 0) {
-            stockMovementService.recordMovement(
-                    savedProduct,
-                    MovementType.IN,
-                    openingQty,
-                    0,
-                    openingQty,
-                    ReferenceType.OPENING_STOCK,
-                    null
-            );
+            log.info("Adding StockMovement: productName={}, afterQty{}, referenceType={}",
+                    savedProduct.getName(), openingQty, ReferenceType.OPENING_STOCK);
+            StockMovement sm =
+                    stockMovementService.recordMovement(
+                            savedProduct,
+                            MovementType.IN,
+                            openingQty,
+                            0,
+                            openingQty,
+                            ReferenceType.OPENING_STOCK,
+                            null
+                    );
+            log.info("StockMovement added successfully with id={}", sm.getId());
         }
+
+        log.info("Product added successfully with id={}", savedProduct.getId());
         return productMapper.toResponseDTO(savedProduct);
     }
 
     public List<ProductResponseDTO> getAllProducts() {
-        log.info("Test: Lombok logging is working");
+        log.info("Fetching all products");
         return productRepository.findAll().stream()
                 .map(productMapper::toResponseDTO)
                 .toList();
     }
 
     public ProductResponseDTO findProductById(Long id) {
-        return productMapper.toResponseDTO(productRepository.findById(id).orElseThrow(()->
-                new ResourceNotFoundException("Product not found with id: "+id)));
+        return productMapper.toResponseDTO(productRepository.findById(id).orElseThrow(() -> {
+            log.info("Product not found, id={}",id);
+            return new ResourceNotFoundException("Product not found, id: "+id);
+        }));
     }
+
     public ProductResponseDTO updateProduct(Long id, Product updatedProduct) {
-        Product existing = productRepository.findById(id).orElseThrow(()->
-                new ResourceNotFoundException("Product not found with id: "+id));
-        if (existing != null) {
-            existing.setName(updatedProduct.getName());
-            existing.setCategory(updatedProduct.getCategory());
-            existing.setPrice(updatedProduct.getPrice());
-            existing.setQuantity(updatedProduct.getQuantity());
-            existing.setUnitOfMeasure(updatedProduct.getUnitOfMeasure());
-            return productMapper.toResponseDTO(productRepository.save(existing));
+        log.info("Updating Product: id={}", id);
+
+        Product existing = productRepository.findById(id).orElseThrow(() -> {
+            log.error("Product not found for update, id={}", id);
+            return new ResourceNotFoundException("Product not found for update, id: " + id);
+        });
+
+        if(!Objects.equals(existing.getQuantity(), updatedProduct.getQuantity()) && updatedProduct.getQuantity()!=null) {
+            log.warn("Product quantity can not be updated, id={}", id);
+            throw new InvalidRequestException("Product quantity can not be updated");
         }
-        return null;
+
+        existing.setName(updatedProduct.getName());
+        existing.setCategory(updatedProduct.getCategory());
+        existing.setPrice(updatedProduct.getPrice());
+        existing.setUnitOfMeasure(updatedProduct.getUnitOfMeasure());
+
+        Product saved = productRepository.save(existing);
+
+        log.info("Product updated successfully with id={}",saved.getId());
+        return productMapper.toResponseDTO(saved);
     }
 
     public void deleteProduct(Long id) {
-        findProductById(id);
-        productRepository.deleteById(id);
+        log.info("Deleting product with id={}",id);
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Product not found for deletion, id={}", id);
+                    return new ResourceNotFoundException("Product not found for deletion, id " + id);
+                });
+
+        productRepository.delete(product);
+
+        log.info("Product deleted successfully, id={}", id);
     }
 
     public List<ProductResponseDTO> searchProducts(String name, String category) {
+        log.info("Searching products with filters: name={}, category={}", name, category);
         List<Product> products;
-        if(name!=null && category!=null) {
+        if (name != null && category != null) {
             products = productRepository.findByNameContainingIgnoreCaseAndCategoryIgnoreCase(name, category);
-        } else if(name!=null) {
+        } else if (name != null) {
             products = productRepository.findByNameContainingIgnoreCase(name);
-        } else if(category!=null) {
+        } else if (category != null) {
             products = productRepository.findByCategoryIgnoreCase(category);
         } else {
             return getAllProducts();
         }
 
+        log.info("Product search result count={}", products.size());
         return products.stream()
                 .map(productMapper::toResponseDTO)
                 .toList();
     }
 
     public Map<String, List<ProductReportDTO>> getProductsGroupedByCategory() {
+        log.info("Fetching category-wise product report");
         List<Product> products = productRepository.findAll();
+        log.info("Total products considered for category report={}", products.size());
 
         return products.stream()
                 .collect(Collectors.groupingBy(Product::getCategory,
